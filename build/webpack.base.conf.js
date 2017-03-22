78cbbcb102848942288f9870ab478c60404eb335
ak <80854284@qq.com>
2017-03-22 16:19:39 +0800

    去掉元素间的空格

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -84,6 +84,7 @@ module.exports = {
     formatter: require('eslint-friendly-formatter')
   },
   vue: {
+    preserveWhitespace: false,
     loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
     postcss: [
       require('autoprefixer')({
