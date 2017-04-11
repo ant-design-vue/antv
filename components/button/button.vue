af892c833e0f97d47eedbf9fdecd35a70a263a5e
ak <80854284@qq.com>
2017-04-11 23:54:09 +0800

    修复button slot 为空时多添加span问题

--- components/button/button.vue
+++ components/button/button.vue
@@ -1,7 +1,7 @@
 <template>
   <button :type="htmlType" :class="classes">
     <i :class="loadingCls" v-if="loading"></i><i :class="typeCls" v-if="icon && !loading"></i>
-    <span><slot></slot></span>
+    <span v-if="$slots.default"><slot></slot></span>
   </button>
 </template>
 <script type="text/babel">
