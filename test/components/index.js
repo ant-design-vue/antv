c02c815a48d2a6437ea651f9df39e44448b228b8
ak <80854284@qq.com>
2017-04-21 18:20:22 +0800

    [del] test

--- test/components/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * 自定义组件需要全局化的在这里指定
- */
-
-import Vue from 'vue'
-import Sidebar from './sidebar'
-
-Vue.component('sidebar', Sidebar)
--- test/directives/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * 自定义指令需要全局化的在这里指定
- */
-
-// import Vue from 'vue'
-// import XXdirective from './XXdirective'
-
-// Vue.component('XXdirective', XXdirective)
